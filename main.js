85ce2d5e0f2524d2b3ed6f2a457f55b2e8c0e013
Thomas Remmer <remmert765@gmail.com>
2022-07-12 22:56:32 +0100

    Start button made and stores player creature stats

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ import deck from "./deck.js"
 import playerHand from "./hand.js"
 
 const container = document.querySelector(".viewport__playerHand")
-const button = document.querySelector(".button")
+const button = document.querySelector(".viewport__button")
+const start = document.querySelector(".viewport__start")
 
-console.log(container)
 
 let randCard = () => {
     let randId = Math.random() * deck.length;
@@ -17,8 +17,6 @@ let drawCard = () => {
     let cardId = randCard();
 
     let chosenCard = deck.filter(card => card.id == cardId)
-    console.table(chosenCard)
-
     chosenCard.forEach(card => {
         container.innerHTML = `<div class="card">
         <img src="${card.sprite}" class="card__image">
@@ -26,10 +24,31 @@ let drawCard = () => {
         <h2 class="card__heading">${card.name.charAt(0).toUpperCase()}${card.name.slice(1)}</h2>
         <p class="card__text"> Attack:${card.attack}  Defence:${card.defence} .</p>
         </div>
-        <div>`
+        <div>` 
+
+        let playerStats = [card.attack, card.defence]
+        console.table(playerStats)
         
     })
 
+
 }
 
+
+
+let gameStart = () => {
+    button.innerText = "End Turn"
+    start.remove()
+    drawCard()
+}
+
+
+
+start.addEventListener("click", gameStart)
 button.addEventListener("click", drawCard)
+
+
+
+
+
+
