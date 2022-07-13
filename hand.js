15b28605bf49b6860f99aae40762e40803a58d73
Thomas Remmer <remmert765@gmail.com>
2022-07-13 23:06:05 +0100

    added all HTML elements/used flex to order them

--- hand.js
+++ hand.js
@@ -1,14 +1,53 @@
-export const playerHand = [
+export const enemyHand = [
     {
-      id: 9,
-      name: "card 1",
-      attack: 1,
-      defence: 1,
-      sprite:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
-    },
-
+        id: 0,
+        name: "Rat",
+        attack: 1,
+        speicalAttack: 3,
+        defence: 1,
+        specialDefence: 4,
+        speed: 5,
+        health: 30,
+        sprite:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/19.png",
+      },
+      {
+        id: 1,
+        name: "Lion",
+        attack: 7,
+        speicalAttack: 5,
+        defence: 2,
+        specialDefence: 2,
+        speed: 3,
+        health: 30,
+        sprite:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/59.png",
+      },
+      {
+        id: 2,
+        name: "Rock",
+        attack: 1,
+        speicalAttack: 1,
+        defence: 5,
+        specialDefence: 5,
+        speed: 1,
+        health: 30,
+        sprite:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/76.png",
+      },
+      {
+        id: 3,
+        name: "Rhino",
+        attack: 5,
+        speicalAttack: 7,
+        defence: 2,
+        specialDefence: 3,
+        speed: 3,
+        health: 30,
+        sprite:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/112.png",
+      },
     
 ];
 
-export default playerHand;
+export default enemyHand;
--- main.js
+++ main.js
@@ -1,51 +1,96 @@
 
 import deck from "./deck.js"
-import playerHand from "./hand.js"
+import enemyHand from "./hand.js"
 
-const container = document.querySelector(".viewport__playerHand")
-const button = document.querySelector(".viewport__button")
-const start = document.querySelector(".viewport__start")
+const playerContainer = document.querySelector(".viewport__playerMonsterContainer")
+const enemyContainer = document.querySelector(".viewport__playspace")
+const button = document.querySelector("#restartButton")
+const start = document.querySelector("#startButton")
+const playerHealthContainer = document.querySelector("#playerHealth")
+const enemyHealthContainer = document.querySelector("#enemyHealth")
+const normalActionButton = document.querySelector("#normalAction")
+const specialActionButton = document.querySelector("#specialAction")
 
+
+
+let playerStats = []
+let aiStats = []
+let playerTurn = true;
 
 let randCard = () => {
-    let randId = Math.random() * deck.length;
-    randId = Math.round(randId)
+    let randId = Math.floor(Math.random() * deck.length);
+
     return randId
 }
 
-let drawCard = () => {
+let playerDrawCard = () => {
     let cardId = randCard();
 
     let chosenCard = deck.filter(card => card.id == cardId)
     chosenCard.forEach(card => {
-        container.innerHTML = `<div class="card">
+        playerContainer.innerHTML = `<div class="card">
         <img src="${card.sprite}" class="card__image">
         <div class="card__content">
         <h2 class="card__heading">${card.name.charAt(0).toUpperCase()}${card.name.slice(1)}</h2>
-        <p class="card__text"> Attack:${card.attack}  Defence:${card.defence} .</p>
+        <p class="card__text"> Attack:${card.attack}  Defence:${card.defence}</p>
+        <p class="card__text"> S Attack:${card.speicalAttack}  S Defence:${card.specialDefence}</p>
         </div>
         <div>` 
-
-        let playerStats = [card.attack, card.defence]
-        console.table(playerStats)
-        
+        playerStats = [card.attack, card.defence, card.speicalAttack, card.specialDefence, card.health, card.speed]
     })
 
+}
+
+let enemyDrawCard = () => {
+    let cardId = randCard();
+
+    let chosenCard = enemyHand.filter(card => card.id == cardId)
+    chosenCard.forEach(card => {
+        enemyContainer.innerHTML = `<div class="card">
+        <img src="${card.sprite}" class="card__image">
+        <div class="card__content">
+        <h2 class="card__heading">${card.name.charAt(0).toUpperCase()}${card.name.slice(1)}</h2>
+        <p class="card__text"> Attack:${card.attack}  Defence:${card.defence}</p>
+        <p class="card__text"> S Attack:${card.speicalAttack} S Defence:${card.specialDefence}</p>
+        </div>
+        <div>` 
+        aiStats = [card.attack, card.defence, card.speicalAttack, card.specialDefence, card.health, card.speed]
+    })
 
 }
 
+const battleStart = (pS, aS) => {
 
+    playerHealthContainer.innerHTML = `<p>Player Health: ${pS[4]}</p>`
+    enemyHealthContainer.innerHTML = `<p>Enemy Health: ${aS[4]}</p>`
+    if (pS[5] >= aS[5]) {
+        console.log("player is faster")
+        playerTurn = true
+    }
+    else {
+        console.log("enemy is faster")
+        playerTurn = false
+    }
+
+}
+
+let nextRound = () => {
+    playerDrawCard()
+    enemyDrawCard()
+    battleStart(playerStats, aiStats)
+}
 
 let gameStart = () => {
-    button.innerText = "End Turn"
+    button.innerText = "Restart"
     start.remove()
-    drawCard()
+    nextRound()
 }
+
 
 
 
 start.addEventListener("click", gameStart)
-button.addEventListener("click", drawCard)
+button.addEventListener("click", nextRound)
 
 
 
